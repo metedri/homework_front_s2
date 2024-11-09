5f979adf5b668af2201af3adff893cea4a64573e
metedri <mitedria@gmail.com>
2024-11-09 20:26:53 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,30 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let copySate = [...state]
+            if (action.payload === 'up'){
+                copySate.sort((a, b) => {
+                   if(a.name > b.name) {
+                    return 1
+                   }
+                   return -1
+                })
+            }
+            if (action.payload === 'down'){
+                copySate.sort((a, b) => {
+                    if(a.name < b.name) {
+                     return 1
+                    }
+                    return -1
+                 })
+            }
+            return copySate // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u=> u.age>18) // need to fix
         }
         default:
             return state
