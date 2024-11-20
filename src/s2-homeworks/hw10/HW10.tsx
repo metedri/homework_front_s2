641be85b7d4c78e48c4b1178adb0af06c24987ae
metedri <mitedria@gmail.com>
2024-11-20 18:16:03 +0300

    HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,13 +14,12 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+    const setLoading = () => { 
+        dispatch(loadingAC(!isLoading))
+        setTimeout(()=>{dispatch(loadingAC(false))}, 1500)
     }
 
     return (
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,21 +2,29 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionType): State => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
-}
-
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+
+type LoadingActionType = {
+    type: 'CHANGE_LOADING'
+    isLoading: boolean
+}
+
+type ActionType = LoadingActionType
+
+type State = {
+    isLoading: boolean
+}
